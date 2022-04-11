ba487604e5dfd5400b9a2a38d5aed7590f4decfd
jasonchong0401 <jason0401chong@gmail.com>
2022-04-11 13:56:43 +0800

    add test_subtract

--- test/test_addition.py
+++ test/test_addition.py
@@ -7,6 +7,6 @@ def test_add():
     result = add(3, 4)
     assert result == 7
 
-#def test_add_string():
-#    with pytest.raises(TypeError):
-#        add("String", 4)
+def test_add_string():
+    with pytest.raises(TypeError):
+        add("String", 4)
---
+++ test/test_subtract.py
@@ -0,0 +1,9 @@
+from src.calculator import subtract
+
+def test_subtract():
+    result = subtract(8, 3)
+    assert result == 5
+
+def test_subtractNew():
+    result = subtract(8.0, 3)
+    assert result == 5.0
